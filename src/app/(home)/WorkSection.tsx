3549b23980242e9865d93b01ae20db9784c5483e
max randle <maxrandle95@gmail.com>
2026-04-24 23:55:18 +1200

    tweak work style adjectives

--- src/app/(home)/WorkSection.tsx
+++ src/app/(home)/WorkSection.tsx
@@ -2,17 +2,9 @@ import { Container } from "@ui/Container";
 import { Heading } from "@ui/Heading";
 import { Section } from "@ui/Section";
 import { Typography } from "@ui/Typography";
-import { FiZap } from "react-icons/fi";
+import { FiSmartphone } from "react-icons/fi";
 
 export const WorkSection: React.FC = () => {
-  const adjectives = [
-    "fast",
-    "responsive",
-    "accessible",
-    "functional();",
-    "seamless",
-    "beautiful",
-  ];
   return (
     <Section spacing="lg">
       <Container className="space-y-20">
@@ -20,25 +12,33 @@ export const WorkSection: React.FC = () => {
         <Typography className="text-center" level="subheading">
           I build
         </Typography>
-        <p className="text-5xl sm:text-7xl text-primary-800 dark:text-primary-500">
-          <span className="flex italic justify-center">
-            Fa
-            <FiZap />t
-          </span>
-        </p>
-        <p className="text-5xl sm:text-7xl text-center">
-          <span className="text-white dark:text-black bg-primary-800 dark:bg-primary-500 px-2">
-            Accessible
-          </span>
-        </p>
-        <p className="text-4xl sm:text-6xl text-center text-primary-800 dark:text-primary-500 tracking-tighter">
+        <div className="space-y-8">
+          <p className="text-6xl sm:text-8xl text-center italic text-primary-800 dark:text-primary-500 font-geom tracking-wider">
+            Fast
+          </p>
+          <p className="text-5xl sm:text-7xl text-center">
+            <span className="text-white dark:text-black bg-primary-800 dark:bg-primary-500 px-2">
+              Accessible
+            </span>
+          </p>
           <pre>
-            <code>functional();</code>
+            <code>
+              <p className="text-4xl sm:text-6xl text-center text-primary-800 dark:text-primary-500 tracking-tighter">
+                functional();
+              </p>
+            </code>
           </pre>
-        </p>
-        <p className="text-5xl sm:text-7xl text-center text-primary-800 dark:text-primary-500 font-serif">
-          Beautiful
-        </p>
+          <p className="text-7xl sm:text-9xl text-center text-primary-800 dark:text-primary-500 font-italianno leading-[0.8]">
+            Beautiful
+          </p>
+          <p className="text-5xl sm:text-7xl text-center text-primary-800 dark:text-primary-500">
+            <span className="flex justify-center">
+              Resp
+              <FiSmartphone className="-mx-1 sm:-mx-2" />
+              nsive
+            </span>
+          </p>
+        </div>
         <Typography className="text-center" level="subheading">
           user interfaces
         </Typography>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,33 @@
 import type { Metadata } from "next";
 import "../tw-theme.scss";
+import { UnifrakturMaguntia, Italianno, Geom, Bangers } from "next/font/google";
+import { cn } from "tailwind-variants";
 
 export const metadata: Metadata = {
   title: "Max Randle",
   description: "Max Randle portfolio website",
 };
+
+const unifrakturMaguntia = UnifrakturMaguntia({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-unifraktur-maguntia",
+});
+const italianno = Italianno({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-italianno",
+});
+const geom = Geom({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-geom",
+});
+const bangers = Bangers({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-bangers",
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +36,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={cn(
+          unifrakturMaguntia.variable,
+          italianno.variable,
+          geom.variable,
+          bangers.variable,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
